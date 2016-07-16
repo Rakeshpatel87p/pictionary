2ffbf82e9dd3ceb0c2a56c003b1297ed9345e541
Rakesh Patel <emailme@rakeshrpatel.com>
2016-07-16 12:47:31 -0400

    new upload

--- public/main.js
+++ public/main.js
@@ -1,9 +1,12 @@
 var socket = io();
 var drawing = false;
+
 var pictionary = function() {
     var canvas, context;
     var guessBox;
-
+    // How to confine drawing to one client?
+    // How to clear drawing space after game has finished?
+    // How to load image for newcomers to the game?
     var draw = function(position) {
         context.beginPath();
         context.arc(position.x, position.y,
@@ -19,12 +22,16 @@ var pictionary = function() {
         var userGuess = guessBox.val();
         socket.emit('guess', userGuess);
         guessBox.val('');
-
     };
 
-    socket.on('guessAppend', function(userGuess){
-    	$('#guesses').append('<div>' + userGuess + '</div>');
-    })
+    socket.on('drawerWord', function(WORDS) {
+        $('#drawerInstructions').show()
+        $('#drawerWord').text(WORDS);
+    });
+
+    socket.on('guessAppend', function(userGuess) {
+        $('#guesses').append('<div>' + userGuess + '</div>');
+    });
 
     guessBox = $('#guess input');
     guessBox.on('keydown', onKeyDown);
@@ -35,33 +42,52 @@ var pictionary = function() {
     canvas[0].height = canvas[0].offsetHeight;
     canvas.mousedown(function() {
         drawing = true;
+
     });
 
     canvas.on('mouseup', function(event) {
         drawing = false;
-    })
+    });
 
-    // Better to place within mousemove or outside?
-    // How to have pre-drawing loaded for newcoming users?
-    socket.on('drawEvent', function(position) {
-        draw(position);
+    socket.on('drawEvent', function(user) {
+        if (user.canDraw) {
+            draw(user.position);
+        }
     });
 
     canvas.on('mousemove', function(event) {
-        if (drawing == true) {
+        // if (drawing == true) {
             var offset = canvas.offset();
             var position = {
                 x: event.pageX - offset.left,
                 y: event.pageY - offset.top
             };
-            draw(position);
+
             // Necessary to have this socket here?
             socket.emit('connection');
-            socket.emit('draw', position);
-        };
+            // on top of sending position, send user identity
+            
+            socket.on('user', function(users) {
+                console.log('users', users);
+                for (var i=0; i < users.length; i++) {
+                    var user = users[i];
+                    console.log((user.canDraw && user.socket_id == socket.id), "CONDITION");
+                    console.log("Can draw?", user.canDraw);
+                    console.log('id on obj', user.socket_id);
+                    console.log("SOCKET ID", socket.id)
+                    if (user.canDraw && user.socket_id == socket.id) {
+                        socket.emit('draw', position);
+                        draw(position);
+                        return;
+                    }
+                }
+                // console.log('user in client', user);
+
+            })
+
+        // };
 
     });
-
 };
 
 $(document).ready(function() {
--- server.js
+++ server.js
@@ -1,3 +1,18 @@
+var WORDS = [
+    "word", "letter", "number", "person", "pen", "class", "people",
+    "sound", "water", "side", "place", "man", "men", "woman", "women", "boy",
+    "girl", "year", "day", "week", "month", "name", "sentence", "line", "air",
+    "land", "home", "hand", "house", "picture", "animal", "mother", "father",
+    "brother", "sister", "world", "head", "page", "country", "question",
+    "answer", "school", "plant", "food", "sun", "state", "eye", "city", "tree",
+    "farm", "story", "sea", "night", "day", "life", "north", "south", "east",
+    "west", "child", "children", "example", "paper", "music", "river", "car",
+    "foot", "feet", "book", "science", "room", "friend", "idea", "fish",
+    "mountain", "horse", "watch", "color", "face", "wood", "list", "bird",
+    "body", "dog", "family", "song", "door", "product", "wind", "ship", "area",
+    "rock", "order", "fire", "problem", "piece", "top", "bottom", "king",
+    "space"
+];
 var http = require('http');
 var express = require('express');
 var socket_io = require('socket.io');
@@ -7,21 +22,69 @@ app.use(express.static('public'));
 
 var server = http.Server(app);
 var io = socket_io(server);
+var users = [];
 
 io.on('connection', function(socket) {
+    if (users.length == 0) {
+        io.sockets.connected[socket.id].emit('drawerWord', selectedWord());
+    };
+    var user = { canDraw: false, socket_id: socket.id.split('').splice(2).join('') };
+    if (users.length > 0){
+        // there are already existing users, then push a user who can't draw
+        users.push(user);    
+    } else {
+        // there are no existing users, then push a user who can draw
+        user.canDraw = true;
+        users.push(user);    
+    }  
+    socket.emit('user', users);
+    console.log(users, 'ALl users');
+    // for (var i = 0; i < users.length; i++) {
+    //     drawing = false;
+    // }
+
     socket.on('draw', function(position) {
-        socket.broadcast.emit('drawEvent', position);
+        // Sitauations
+        /*
+        - user stays on browser too long and forgets to quit
+        - are we fair?
+        - 2 player first then multiple!
+        */
+        // who is drawing??
+        // console.log(users[0], 'USER zero');
+        console.log("SOCKET ID", socket.id);
+
+        if (socket.id == users[0].socket_id && users[0].canDraw) {
+            socket.broadcast.emit('drawEvent', {position:position, socket_id: users[0].socket_id, canDraw: users[0].canDraw});
+        };
     });
 
     socket.on('guess', function(userGuess) {
-        console.log(userGuess);
         io.emit('guessAppend', userGuess);
+        if (userGuess = WORDS[randomNumber]) {
+            console.log('you got it!');
+        };
+    });
+
+    socket.on('disconnect', function() {
+        for (var i = 0; i < users.length; i++) {
+            if (users[i].socket_id === socket.id) {
+                console.log('Users Socket_ID from Obj',users[i].socket_id); 
+                console.log('Socket ID ', socket.id);
+                users.splice(i, 1);
+
+                if (i == 0) {
+                    io.sockets.connected[users[0].socket_id].emit('drawerWord', selectedWord());
+                };
+            };
+        };
     });
 
 });
-// create a new socket.
-// io.on('guessBox', function(socket) {
 
-// });
+var selectedWord = function() {
+    randomNumber = Math.floor((Math.random() * 100) + 1);
+    return WORDS[randomNumber]
+};
 
 server.listen(8080);
